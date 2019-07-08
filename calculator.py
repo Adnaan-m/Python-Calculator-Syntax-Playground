8721cf7f0a5855320db4ab07cd2988b568b4ed5f
hodaccadm <hodaccadm@DAS00926.local>
2019-07-08 10:58:21 +0100

    Advanced and Standard Python Calc completed

--- calculator.py
+++ calculator.py
@@ -1,30 +1,65 @@
-# while print exit is not true, do below 
-# else exit
-
+import math  
+def standard_calc():
+    operator = raw_input()
+    if operator.upper() == "A":
+        print x + y 
+    elif operator.upper() == "S":
+        print x - y 
+    elif operator.upper() == "D":
+        # if x == 0 or y == 0:
+        #     print "ERROR 00: Rhys likes eggs and bugs but not computers"
+        # else:
+            print "%.2f" %(x / y) 
+    elif operator.upper() == "M":
+        print x * y 
+    else:
+        print "Incorrect Operator"
 
 print "Welcome to Calculator 101"
-name = raw_input("What is you're name?")
+name = raw_input("What is you're name? ")
 print "Thank you" , name ,", let's get cracking!"
+        
+Restart = True
+while Restart == True:
+    option = raw_input("Would you like a Standard Calculator or Advanced? [S] or [A] ")
 
+    if option.upper() == "S":
+        x = float(input ("First Integar: "))
+        y = float(input ("Second Integar: "))
+        print "What would you like to do with these two numbers", name,"?"
+        print "Type [A] to add, [S] to subtract, [D] to divide, [M] to multiply"
+        
+        try:
+            standard_calc()
+        except:
+            print "Error 00: Cannot Calculate Total"
 
+        Replay = raw_input("Would you like to start again? Y / N: ")
+        if Replay.upper() == "N":
+            Restart = False
+        elif Replay.upper() == "Y":
+            Restart = True
 
-###########################################
-x = float(input ("Type the first Integar"))
-y = float(input ("Type the Second Integar"))
-print "What would you like to do with these two number?"
-print "Type [A] to add, [S] to subtract, [D] to divide, [M] to multiply"
+    elif option.upper() == "A":
+        print "Type [R] to Square Root Or [P] to measure the Power of a Value"
+        operator = raw_input()
 
-operator = raw_input()
+        if operator.upper() == "R":
+            x = float(input ("Square Root of: "))
+            print math.sqrt(x)
+            
+        elif operator.upper() == "P":
+            x = float(input ("First Integar: "))
+            y = float(input ("To the Power of: "))
+            powr = pow(x, y)
 
+            print powr 
 
-if operator.upper() == "A":
-    print x + y 
-elif operator.upper() == "S":
-    print x - y 
-elif operator.upper() == "D":
-    print "%.2f" %(x / y) 
-elif operator.upper() == "M":
-    print x * y 
+        Replay = raw_input("Would you like to start again? Y / N: ")
+        if Replay.upper() == "N":
+            Restart = False
+        elif Replay.upper() == "Y":
+            Restart = True
 else:
-    print "Incorrect Operator"
-###########################################        
+    print "Goodbye"
+
